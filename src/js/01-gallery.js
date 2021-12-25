f2a426af0d82370749d34d70a63798b911517dc4
mryevhenii07 <mrit.works07@gmail.com>
2021-12-25 18:00:55 +0200

    the first work is finished

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,8 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
 const refs = {
